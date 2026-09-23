1ee1750842da020ad3bdabab938be1a771e15f9f
Duke-Daniel <drecord29@setoncatholic.org>
2026-09-23 20:14:27 +0000

    calculater finished

---
+++ main.py
@@ -0,0 +1,24 @@
+# this funtion add two numbers
+def add(x,y):
+    print(x + y)
+
+# this funtion subtracts two numbers
+def sub(x,y):
+    print(x - y)
+
+# this funtion multiplies two numbers
+def mul(x,y):
+    print(x * y)
+
+# this funtion divids  two numbers
+def div(x,y):
+    print(x/y)
+####################################################################
+###############3
+x = int(input("enter your first number: "))
+y = int(input("eneter your second number: "))
+
+add(x,y) 
+sub(x,y) 
+mul(x,y) 
+div(x,y) 
